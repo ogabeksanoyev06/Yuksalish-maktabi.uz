7dc41ed4e79f8d74850ab38bbc90a031e86d1dba
ogabeksanoyev06 <ogabeksanoyev06@gmail.com>
2023-06-24 15:39:40 +0500

    ..

--- src/views/site/contact/index.vue
+++ src/views/site/contact/index.vue
@@ -49,6 +49,15 @@
         >
           Biz bilan farzandingizni kelajagini mustahkam quring
         </app-text>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1498.5236520612602!2d69.2180579399769!3d41.30783452689444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b1f8365179b%3A0xa2d2db9aa56870c0!2sYuksalish%20maktabi!5e0!3m2!1suz!2s!4v1687601929534!5m2!1suz!2s"
+          height="550"
+          style="border-radius: 20px; width: 100%"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="mb-30"
+        ></iframe>
         <div class="location">
           <div
             style="background-color: #ececec; border-radius: 16px"
--- src/views/site/teacher/index.vue
+++ src/views/site/teacher/index.vue
@@ -18,14 +18,39 @@
           :size="isMobile ? 32 : 48"
           :line-height="isMobile ? 40 : 64"
           weight="700"
-          class="mb-20 color-orange-gradient"
+          class="mb-30 color-orange-gradient"
           data-aos="fade-right"
         >
           Malakali ustozlar
         </app-text>
         <div class="wrapper">
-          <div class="wrapper__item">
-            <img src="" alt="" />
+          <div
+            class="wrapper__item"
+            v-for="(item, i) in 10"
+            :key="i"
+            data-aos="fade-up"
+            :data-aos-duration="(i + 1) * 100"
+          >
+            <img
+              src="/images/muhammadalieshonqulov1.png"
+              class="mb-20"
+              style="max-width: 100%; height: auto"
+            />
+            <app-text
+              :size="isMobile ? 24 : 32"
+              :line-height="isMobile ? 32 : 40"
+              weight="600"
+              class="mb-10"
+            >
+              Muhammadali Eshonqulov
+            </app-text>
+            <app-text
+              :size="isMobile ? 18 : 20"
+              :line-height="isMobile ? 24 : 26"
+              weight="500"
+            >
+              O'qituvchilar
+            </app-text>
           </div>
         </div>
       </div>
@@ -42,3 +67,10 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+}
+</style>
